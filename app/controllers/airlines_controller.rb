67cda4bd07f6fa4cb49cfac26cfff6019a872f34
bwesterg <benton.westergaard@gmail.com>
2021-07-20 16:00:24 -0600

    Delete function added, and connected to frontend.

--- app/controllers/airlines_controller.rb
+++ app/controllers/airlines_controller.rb
@@ -5,4 +5,23 @@ class AirlinesController < ApplicationController
         render json: @airlines
     end
 
+    def create
+        @airline = Airline.create(
+            name: params[:name],
+            image: params[:image],
+            review: params[:review],
+            rating: params[:rating],
+            high_marks: params[:high_marks]
+        )
+        render json: @airline, status: :created
+    end
+
+    def destroy 
+        @airline = Airline.find(params[:id])
+
+        @airline.destroy
+        
+        render status: :no_content
+    end
+
 end
