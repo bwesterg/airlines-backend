170270f67f474dd1da93de05d2cd9aeed0bd65a1
bwesterg <benton.westergaard@gmail.com>
2021-07-28 17:09:59 -0600

    Added a user controller.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_18_230542) do
+ActiveRecord::Schema.define(version: 2021_07_28_200329) do
 
   create_table "airlines", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2021_07_18_230542) do
     t.string "review"
     t.integer "rating"
     t.boolean "high_marks"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
